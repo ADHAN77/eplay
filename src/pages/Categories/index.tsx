a631929c69de642caa4d052cd51eacdd6a668014
adhan <adhanmont36@gmail.com>
2024-10-22 15:40:43 -0300

    Base back and v1.0

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,37 @@
+import ProductsList from '../../components/ProductsList'
+
+import {
+  useGetActionGamesQuery,
+  useGetFightGamesQuery,
+  useGetRpgGamesQuery,
+  useGetSimulationsGamesQuery,
+  useGetSportGamesQuery
+} from '../../services/api'
+
+const Categories = () => {
+  const { data: actionGames } = useGetActionGamesQuery()
+  const { data: fightGames } = useGetFightGamesQuery()
+  const { data: rpgGames } = useGetRpgGamesQuery()
+  const { data: simulationGames } = useGetSimulationsGamesQuery()
+  const { data: sportGames } = useGetSportGamesQuery()
+
+  if (actionGames && fightGames && rpgGames && simulationGames && sportGames) {
+    return (
+      <>
+        <ProductsList games={actionGames} title="Ação" background="black" />
+        <ProductsList games={sportGames} title="Esportes" background="gray" />
+        <ProductsList games={fightGames} title="Luta" background="black" />
+        <ProductsList games={rpgGames} title="RPG" background="gray" />
+        <ProductsList
+          games={simulationGames}
+          title="Simulação"
+          background="black"
+        />
+      </>
+    )
+  }
+
+  return <h4>Carregando...</h4>
+}
+
+export default Categories
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,51 @@
+import Banner from '../../components/Banner'
+import ProductsList from '../../components/ProductsList'
+
+import { useGetOnsaleQuery, useGetSoonQuery } from '../../services/api'
+
+export interface GalleryItem {
+  type: 'image' | 'video'
+  url: string
+}
+
+export type Game = {
+  id: number
+  name: string
+  description: string
+  release_date?: string
+  prices: {
+    discount?: number
+    old?: number
+    current?: number
+  }
+  details: {
+    category: string
+    system: string
+    developer: string
+    publisher: string
+    languages: string[]
+  }
+  media: {
+    thumbnail: string
+    cover: string
+    gallery: GalleryItem[]
+  }
+}
+
+const Home = () => {
+  const { data: onSaleGames } = useGetOnsaleQuery()
+  const { data: soonGames } = useGetSoonQuery()
+
+  if (onSaleGames && soonGames) {
+    return (
+      <>
+        <Banner />
+        <ProductsList games={onSaleGames} title="Pormoções" background="gray" />
+        <ProductsList games={soonGames} title="Em breve" background="black" />
+      </>
+    )
+  }
+
+  return <h4>Carregando...</h4>
+}
+export default Home
